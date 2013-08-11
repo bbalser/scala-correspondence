d80479e696d756f3735c9f7b7f2f5b35df44a781
Rob Conaway <rob@conaway58.com>
2013-08-11 18:18:29 -0400

    :A first commit after creating the project with activator (Hello, Scala) and adding the first test. I've added almost everything to the repo. Some of the .idea stuff is questionable; we'll see if it causes Brian any problems. Ideally, Brian will just clone to his machine, start Idea, and be up and running. I'm using Idea 12.1.4 with JDK 1.6.0_51.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := """fizz-buzz-1"""
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
---
+++ src/test/scala/FizzBuzzSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+
+class FizzBuzzSpec extends FunSuite with ShouldMatchers {
+
+  def fizzbuzz(n: Int):String = ""
+
+  test("1 returns 1") {
+    fizzbuzz(1) should be ("1")
+  }
+
+}
