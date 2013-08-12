9b0258f6471fe5f95dcff69ef577141c4aa268c1
Brian Balser <brian.balser@gmail.com>
2013-08-12 09:07:30 -0400

    Passed test by returning 1

--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -3,10 +3,12 @@ import org.scalatest.matchers.ShouldMatchers
 
 class FizzBuzzSpec extends FunSuite with ShouldMatchers {
 
-  def fizzbuzz(n: Int):String = ""
+  def fizzbuzz(n: Int):String = "1"
 
   test("1 returns 1") {
     fizzbuzz(1) should be ("1")
   }
+
+
 
 }
