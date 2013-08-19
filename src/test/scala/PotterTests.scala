f20b96e9b8dbb9ee573de1948d9e4122146a5985
Rob Conaway <rob@conaway58.com>
2013-08-19 16:03:19 -0400

    Passed "price of 1 book is 8" Added "price of 2 of the same book is 16"

--- src/test/scala/PotterTests.scala
+++ src/test/scala/PotterTests.scala
@@ -3,8 +3,14 @@ import org.scalatest.matchers.ShouldMatchers
 
 class PotterTests extends FunSuite with ShouldMatchers {
 
+  def determinePrice(books:List[Int]):Double = 8
+
   test("price of 1 book is 8") {
     determinePrice(List(1)) should be (8)
+  }
+
+  test("price of 2 of the same book is 16") {
+    determinePrice(List(1,1)) should be (16)
   }
 
 }
